0c2e283ee6b19b1c9402a06ffa36f862d0729c77
abizargiffari80 <abizargiffari80@gmail.com>
2025-09-24 23:26:00 +0700

    update

--- cypress/e2e/integration/positive_case/login.cy.js
+++ cypress/e2e/integration/positive_case/login.cy.js
@@ -1,40 +1,30 @@
-// cypress/integration/positive_case/login.cy.js
-
 describe('Login and Checkout Tests', () => {
 
   const testData = {
     baseUrl: 'https://recruitment-staging-queenbee.paradev.io',
     userCredentials: {
-      phoneNumber: '081234567890',  // Dynamic phone number for login
-      password: 'Password123',       // Dynamic password for login
-      verificationCode: '123456'    // Dynamic verification code for login
+      phoneNumber: '081234567890',
+      password: 'Password123',
+      verificationCode: '123456'
     },
-    promoCode: 'QRP-TEST-123'  // Valid promo code for testing
+    promoCode: 'QRP-TEST-123'
   };
 
-  // Step 1: Handle closing ads by clicking outside
+  // Handle closing ads by clicking outside
   before(() => {
     cy.visit(testData.baseUrl);
+    cy.wait(5000);
     cy.get('body').click(10, 10);  // Click near the top-left corner to close the ad
-    cy.wait(2000);  // Wait for 2 seconds to ensure ad is closed before proceeding
+    cy.wait(5000);  // Wait for 2 seconds to ensure ad is closed before proceeding
   });
 
-  // Reusable function to login
-  const login = () => {
-    // Click the "Masuk" (Login) button
-    cy.get('a[href="/login"]').click();  // Selector using class name and anchor tag
 
-    // Input phone number and password
+  const login = () => {
+    cy.get('a[href="/login"]').click();
     cy.get('#page-login__tabs-number__input-number').type(testData.userCredentials.phoneNumber);
     cy.get('#page-login__tabs-email__input-password').type(testData.userCredentials.password);
-
-    // Click the login button
     cy.get('#page-login__button-login').click();
-
-    // Input the verification code
-    cy.get('#page-login__tabs-number__input-verification-code').type(testData.userCredentials.verificationCode);  // Add verification code input field here
-
-    // Assert that the user is logged in (check if the URL includes '/dashboard')
+    cy.get('#page-login__tabs-number__input-verification-code').type(testData.userCredentials.verificationCode);
     cy.url().should('include', '/dashboard');
   };
 
